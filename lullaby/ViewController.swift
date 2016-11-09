d0239103769c1de9097b9c1ff53dd329b0b30bbd
“zdenham” <zdenham@usc.edu>
2016-11-08 20:42:46 -0800

    Initial Commit

---
+++ lullaby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lullaby
+//
+//  Created by Zachary Denham on 11/8/16.
+//  Copyright © 2016 scope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
