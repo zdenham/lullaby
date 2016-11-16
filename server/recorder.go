ac71eb1ce44e37dce26a540a32ff6fc1dd820012
Myrl Marmarelis <myrl@Myrls-MacBook-Pro.local>
2016-11-15 21:48:16 -0800

    woooo

--- server/recorder.go
+++ server/recorder.go
@@ -5,6 +5,8 @@ import (
  "os"
  "bufio"
  "log"
+ "fmt"
+ "strconv"
 )
 
 const (
@@ -12,36 +14,44 @@ const (
  PORT = "7000"
 )
 
-func getString(scanner *bufio.Scanner) string {
- if !scanner.Scan() {
-  return ""
- } else {
-  return string(scanner.Bytes())
+func handleRequest(conn net.Conn, id int) {
+ name := strconv.Itoa(id) + ".csv"
+ fmt.Println("Creating new file:", name)
+ defer func() {
+  fmt.Println("Closing file:", name)
+ }()
+ file, err:= os.Create("data/"+name)
+ if err != nil {
+  fmt.Println("Couldn't open file:", name)
+  return
  }
-}
-
-func handleRequest(conn net.Conn) {
-  scanner := bufio.NewScanner(conn) // reads lines
-  scanner.Scan()
-  name := getString(scanner)
-  for {
-
+ defer file.Close()
+ scanner := bufio.NewScanner(conn) // reads lines
+ for {
+  if !scanner.Scan() {
+   return
   }
+  line := string(scanner.Bytes()) + "\n"
+  if _, err := file.Write([]byte(line)); err != nil {
+   return
+  }
+ }
 }
 
 func main() {
- listener, err := net.Listen("tcp", CONN_HOST+":"+CONN_PORT)
+ listener, err := net.Listen("tcp", HOST+":"+PORT)
  if err != nil {
-  log.Fatal("Error listening: ", err.Error())
+  log.Fatal("Error listening:", err.Error())
  }
  defer listener.Close()
- fmt.Println("Listening on " + HOST + ":" + PORT)
+ i := 1
  for {
-  conn, err := l.Accept()
+  conn, err := listener.Accept()
   if err != nil {
-   log.Fatal("Error accepting: ", err.Error())
+   log.Fatal("Error accepting:", err.Error())
   }
-  go handleRequest(conn)
+  go handleRequest(conn, i)
   defer conn.Close()
+  i += 1
  }
 }
