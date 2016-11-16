7cfdc30f2cc568909eae649b4606dd6aaa30c4f3
“zdenham” <zdenham@usc.edu>
2016-11-15 20:39:48 -0800

    made a new view

---
+++ lullaby/AccellerometerView.swift
@@ -0,0 +1,23 @@
+//
+//  AccellerometerView.swift
+//  lullaby
+//
+//  Created by Zachary Denham on 11/15/16.
+//  Copyright © 2016 scope. All rights reserved.
+//
+
+import UIKit
+
+class AccellerometerView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
