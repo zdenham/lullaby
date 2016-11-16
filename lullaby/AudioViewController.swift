e9cab47bb92351756c8bab48919dc4aa9e0e7731
NiwradMendyke <mendyke@usc.edu>
2016-11-15 20:19:07 -0800

    built frontend stuff

--- lullaby/AudioViewController.swift
+++ lullaby/AudioViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class AudioViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var audioTable: UITableView!
+    
+    var selectedStory = "War and Peace"
+    
+    var titleList: [String] = ["War and Peace", "Cote on Recursion", "Cote on Pink Elephants", "Cote on Induction", "United States Constitution", "Counting sheep", "Green Eggs and Ham", "Romeo and Juliet", "Wuthering Heights"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,13 +24,13 @@ class AudioViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 20;
+        return 9;
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = audioTable.dequeueReusableCell(withIdentifier: "audioCell", for: indexPath) 
         
-        cell.textLabel?.text = "row \(indexPath.row)"
+        cell.textLabel?.text = titleList[indexPath.row]
         
         print("row \(indexPath.row)")
         
@@ -37,5 +42,18 @@ class AudioViewController: UIViewController, UITableViewDataSource, UITableViewD
         // Dispose of any resources that can be recreated.
     }
     
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selectStory called")
+        
+        selectedStory = titleList[indexPath.row]
+        
+        print("selectedStory is " + selectedStory)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("about to segue")
+        let mvc = segue.destination as! MainViewController
+        
+        mvc.storyTitle = selectedStory
+    }
 }
